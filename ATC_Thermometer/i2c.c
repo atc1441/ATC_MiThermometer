b6b09ae997fa1e4054132131eb3ffeb53a1395ba
atc1441 <1102694+atc1441@users.noreply.github.com>
2020-09-01 22:03:45 +0200

    Add files via upload

--- ATC_Thermometer/i2c.c
+++ ATC_Thermometer/i2c.c
@@ -8,6 +8,7 @@
 void init_i2c(){
 	i2c_gpio_set(I2C_GPIO_GROUP_C2C3); 
 	i2c_master_init(0x78, (uint8_t)(CLOCK_SYS_CLOCK_HZ/(4*400000)) );
+    reg_i2c_mode |= FLD_I2C_HOLD_MASTER;// Enable clock stretching for Sensor
 }
 
 void send_i2c(uint8_t device_id, uint8_t *buffer, int dataLen){
