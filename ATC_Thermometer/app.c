987645f36e500022deadbe44374a25ee401193e5
atc1441 <1102694+atc1441@users.noreply.github.com>
2020-10-02 20:50:57 +0200

    Update app.c

--- ATC_Thermometer/app.c
+++ ATC_Thermometer/app.c
@@ -34,7 +34,7 @@ void user_init_normal(void){//this will get executed one time after power up
 	battery_mv = get_battery_mv();
 	battery_level = get_battery_level(get_battery_mv());
 	
-	uint8_t read=0x00;#//ERASE THE MI ID to prevent blocking :D
+	uint8_t read=0x00;//ERASE THE MI ID to prevent blocking :D
 	flash_write_page(0x78000, 1, read);
 	if(read != 0xff)
 	flash_erase_sector(0x78000);
