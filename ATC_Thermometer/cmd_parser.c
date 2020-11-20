f3a21b7b7edfa871f8a40a7257b9b5d1436ae011
atc1441 <1102694+atc1441@users.noreply.github.com>
2020-11-20 06:53:49 +0100

    Revert "Use enum for smiley type"

--- ATC_Thermometer/cmd_parser.c
+++ ATC_Thermometer/cmd_parser.c
@@ -29,15 +29,15 @@ void cmd_parser(void * p){
 	}else if(inData == 0xA0){
 		blinking_smiley = false;
 		comfort_smiley = false;
-        show_smiley(Smiley_off);
+		show_smiley(0);//Smiley off
 	}else if(inData == 0xA1){
 		blinking_smiley = false;
 		comfort_smiley = false;
-        show_smiley(Smiley_happy);
+		show_smiley(1);//Smiley happy
 	}else if(inData == 0xA2){ 
 		blinking_smiley = false;
 		comfort_smiley = false;
-        show_smiley(Smiley_sad);
+		show_smiley(2);//Smiley sad
 	}else if(inData == 0xA3){
 		blinking_smiley = false;
 		comfort_smiley = true; // Comfort Indicator
@@ -56,9 +56,9 @@ void cmd_parser(void * p){
 		if(humi_offset<-50)humi_offset=-50;
 		if(humi_offset>50)humi_offset=50;
 	}else if(inData == 0xFC){
-        //Set temp alarm point value divided by 10 for temp in °C
-        temp_alarm_point = req->dat[1] == 0 ? 1 : req->dat[1];
-    }else if(inData == 0xFD){
+		temp_alarm_point = req->dat[1];//Set temp alarm point value divided by 10 for temp in °C
+		if(temp_alarm_point==0)temp_alarm_point = 1;
+	}else if(inData == 0xFD){
 		humi_alarm_point = req->dat[1];//Set humi alarm point
 		if(humi_alarm_point==0)humi_alarm_point = 1;
 		if(humi_alarm_point>50)humi_alarm_point = 50;
--- ATC_Thermometer/lcd.h
+++ ATC_Thermometer/lcd.h
@@ -3,19 +3,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef enum {
-    Smiley_off,
-    Smiley_happy,
-    Smiley_sad
-} SmileyType_t;
-
 void init_lcd();
 void update_lcd();
 void show_temp_symbol(uint8_t symbol);
 void show_battery_symbol(bool state);
 void show_big_number(int16_t number, bool point);
 void show_small_number(uint16_t number, bool percent);
-void show_smiley(SmileyType_t state);
+void show_smiley(uint8_t state);
 void show_atc_mac();
 void show_ble_symbol(bool state);
 void send_to_lcd_long(uint8_t byte1, uint8_t byte2, uint8_t byte3, uint8_t byte4, uint8_t byte5, uint8_t byte6);
